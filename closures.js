a347e5b762752fd71676895bd6af52717a5f4280
Zac McClung <zacmcclung@Zacs-MacBook-Pro.local>
2017-03-22 15:08:19 -0600

    completed problems up to 7

--- closures.js
+++ closures.js
@@ -146,10 +146,12 @@ function motivation(firstname, lastname) {
   var welcomeText = 'You\'re doing awesome, keep it up ';
 
   // code message function here.
-
+  function message() {
+    return welcomeText + firstname + ' ' + lastname + '.';
+  }
 
   //Uncommment this to return the value of your invoked message function
-  //return message();
+  return message();
 
 }
 
@@ -188,9 +190,14 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: function() {
+      return privateMethod();
+    }
   };
 
 })();
+
+module.publicMethod();
 
 
 
